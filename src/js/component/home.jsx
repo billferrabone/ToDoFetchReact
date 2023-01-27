3e2c78ba66f15d1ff7e5e35deb39f6e0c1d97d61
billferrabone <billferrabone@gmail.com>
2023-01-27 22:23:57 +0000

    codigo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [toDos, setToDos] = useState([
+    "Llamar al abogado",
+    "Lavar el coche",
+    "Preparar maletas",
+  ]);
+  const [newTask, setNewTask] = useState("");
+
+  function addNewTask(e) {
+    if (e.key == "Enter" && newTask != "") {
+      let newToDos = [...toDos];
+      newToDos.push(newTask);
+      setToDos(newToDos);
+      setNewTask("");
+    }
+  }
+
+  function deleteTask(index) {
+    let newToDo = [...toDos];
+    newToDo.splice(index, 1);
+    setToDos(newToDo);
+  }
+
+  return (
+    <body>
+    <div className="container">
+      <h1>To do List</h1>
+      <input
+        className="mb-4"
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        onKeyDown={addNewTask}
+      />
+      <ul className="list-group">
+        {toDos.map((task, index) => (
+          <li key={index} className="list-group-item">
+            <span
+              id="button"
+              onClick={() => deleteTask(index)}
+              className="badge rounded-pill text-bg-danger"
+            >
+              X
+            </span>
+            {task}
+          </li>
+        ))}
+        <li id="footer" className="list-group-item">
+          {toDos.length == 0
+            ? "Great job, you are all done!"
+            : toDos.length + " tasks left, get it done!"}
+        </li>
+      </ul>
+    </div>
+    </body>
+  );
 };
 
 export default Home;
+
